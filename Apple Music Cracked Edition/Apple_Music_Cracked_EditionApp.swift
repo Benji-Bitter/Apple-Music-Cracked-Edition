ad16c6e589508d0ee8b534a7dee9fb48d84a82e3
apmckelvey <apmckelvey@thewildcats.net>
2026-01-06 11:48:53 -0500

    Initial Commit

---
+++ Apple Music Cracked Edition/Apple_Music_Cracked_EditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Music_Cracked_EditionApp.swift
+//  Apple Music Cracked Edition
+//
+//  Created by apmckelvey on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Music_Cracked_EditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Music Cracked Edition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple Music Cracked Edition
+//
+//  Created by apmckelvey on 1/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
